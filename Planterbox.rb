92944f9d981094cef06ea0d0d292c0c01789250b
Cameron Turner <paddlesandskis@gmail.com>
2025-06-18 01:11:51 -0400

    Initial commit of the Planterbox driver.

---
+++ Planterbox.rb
@@ -0,0 +1,30 @@
+# Planterbox.rb
+#
+# Main driver for the Planterbox expert system rules language.
+
+class Planterbox
+
+    def Initialize(name)
+
+        begin
+            puts "Hello"
+
+        rescue => e
+
+        end
+    end
+	
+end
+
+
+begin
+
+    puts "Hello World"
+ 
+
+rescue => e 
+
+    puts "Exception"
+
+end
+
